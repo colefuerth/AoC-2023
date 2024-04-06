6a4a51de0e5d94632aa7cae0b1ccb3810c7ac646
Cole Fuerth <colefuerth@gmail.com>
2024-04-06 00:08:08 -0400

    day08 cleanup

--- day08/AoC.py
+++ day08/AoC.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
 
 import sys
-from copy import deepcopy
 import re
-from typing import List
+from typing import List, Dict
 from math import lcm
 
 
-def part1(f: List[str]) -> int:
-    instructions = [0 if c == 'L' else 1 for c in f[0]]
-    nodes = {match[0]:tuple(match[1:]) for match in [re.findall(r'[A-Z]+', line) for line in f[2:]]}
-    
+def part1(instructions: List[int], nodes: Dict[str, List[str]]) -> int:
     steps = 0
     acc = 'AAA'
     
@@ -19,10 +15,7 @@ def part1(f: List[str]) -> int:
         steps += 1
     return steps
 
-def part2(f: List[str]) -> int:
-    instructions = [0 if c == 'L' else 1 for c in f[0]]
-    nodes = {match[0]:tuple(match[1:]) for match in [re.findall(r'[0-9A-Z]+', line) for line in f[2:]]}
-    
+def part2(instructions: List[int], nodes: Dict[str, List[str]]) -> int:
     steps = 0
     accs = [node for node in nodes if node[2] == 'A']
     multiples = [0 for _ in accs]
@@ -44,5 +37,8 @@ if __name__ == '__main__':
         for l in open(fname, 'r').readlines()
     ]
     
-    print('Part 1:', part1(deepcopy(f)))
-    print('Part 2:', part2(deepcopy(f)))
+    instructions = ['LR'.index(c) for c in f[0]]
+    nodes = {match[0]:tuple(match[1:]) for match in [re.findall(r'[0-9A-Z]+', line) for line in f[2:]]}
+    
+    print('Part 1:', part1(instructions, nodes))
+    print('Part 2:', part2(instructions, nodes))
